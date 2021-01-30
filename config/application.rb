250ed9859234b16da05666c813650bb4ea3d031a
simsalabim08 <westra77@gmail.com>
2021-01-30 12:11:30 +0100

    Refactored strong params for portfolio items

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,8 @@ module DevcampPortfolio
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
+    config.action_controller.permit_all_parameters = true
+
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
