84ebe17ebe84befe97babd83a3c83570218cb66a
simsalabim08 <westra77@gmail.com>
2021-01-30 13:36:07 +0100

    Added nav bar and added default header content

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
   include SetSource
-  include GuestUser
-
-  def current_user
-    super || guest_user
-  end
+  include UserConcern
+  include DefaultPageContent
 end
--- app/controllers/concerns/user_concern.rb
+++ app/controllers/concerns/user_concern.rb
@@ -1,7 +1,11 @@
-module GuestUser
+module UserConcern
   extend ActiveSupport::Concern
 
   def guest_user
     @guest_user = User.new(name: "Guest")
+  end
+
+  def current_user
+    super || guest_user
   end
 end
